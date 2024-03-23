e39d769e6743cf8937e8c5c23088e52a7f5a7d2e
TranNhutTrung1003 <125780142+TranNhutTrung1003@users.noreply.github.com>
2024-03-23 21:51:23 +0700

    xử lí xong sự kiện cho nút có nội dung là đặt lại

--- Cau1_BMI/src/ntu/trannhuttrung_63135935/MyBMI.java
+++ Cau1_BMI/src/ntu/trannhuttrung_63135935/MyBMI.java
@@ -14,6 +14,8 @@ import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -240,6 +242,17 @@ public class MyBMI extends JFrame {
                         }
                     }
                 }
+			}
+        });
+		
+		btnNewButton_1.addActionListener((ActionListener) new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				textField_1.setText("");
+				textField.setText("");
+				lblNewLabel_5.setText("0");
+				lblNewLabel_7.setText("");
 			}
         });
 	}
